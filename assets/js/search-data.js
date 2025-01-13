d91a10c9346fdcc8def8f075501ab5697184b766
VIS-WA <savitha.viswanadh@research.iiit.ac.in>
2025-01-13 09:57:36 +0000

    Deploying to gh-pages from @ VIS-WA/VIS-WA.github.io@4fa1345100c120d83429da8d772fd6e8f232da09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Coming up soon!",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
